8bea926d57775743d5bc41a0dcdba6232db3e4ee
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-03-21 16:52:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,6 @@ locals {
   path = coalesce(var.path, var.name)
   description = coalesce(var.description, "${var.name} Certificate Authority")
   urls_prefix = var.urls_prefix
-  
-  issuing_certificates = [ for url in local.urls_prefix: "${url}/v1/pippo/ca" ]
 }
 
 resource "vault_mount" "this" {
@@ -38,7 +36,6 @@ resource "vault_pki_secret_backend_config_urls" "this" {
   
   backend = vault_pki_secret_backend_root_cert.this.backend
   
-  #issuing_certificates = toset([ for url in local.urls_prefix: "${url}/v1/${vault_mount.this.path}/ca" ])
-  #crl_distribution_points = toset([ for url in local.urls_prefix: "${url}/v1/${vault_mount.this.path}/crl"]
   issuing_certificates = [ for url in local.urls_prefix: "${url}/v1/${vault_mount.this.path}/ca" ]
+  crl_distribution_points = [ for url in local.urls_prefix: "${url}/v1/${vault_mount.this.path}/crl" ]
 }
